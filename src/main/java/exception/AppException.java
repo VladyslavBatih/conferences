dfee6db467b38bccf9c8ba8428c30431bcf6f0be
Vladyslav Batih <vladislavbatig@gmail.com>
2022-09-13 08:24:43 +0300

    Added custom exceptions

---
+++ src/main/java/exception/AppException.java
@@ -0,0 +1,16 @@
+package exception;
+
+public class AppException extends Exception {
+
+    public AppException() {
+        super();
+    }
+
+    public AppException(String message) {
+        super(message);
+    }
+
+    public AppException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
