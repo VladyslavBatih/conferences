ff081c9139ad1518dcf8d582f24c3081e0d578fb
Vladyslav Batih <vladislavbatig@gmail.com>
2022-10-18 21:42:48 +0300

    Change static strings

--- src/main/java/util/DBConstant.java
+++ src/main/java/util/DBConstant.java
@@ -1,9 +1,15 @@
 package util;
 
-public class DBConstant {
+public final class DBConstant {
 
-    public static final String JAVA_COMP_ENV = "java:/comp/env";
-    public static final String JDBC_PROJECT_NAME = "jdbc/conferences";
+    public static final String SQL_INSERT_USER =
+            "INSERT INTO users(login, password, firstname, lastname, role_id) VALUES (?,?,?,?,?)";
+
+    public static final String SQL_UPDATE_USER = "UPDATE users SET firstname=?, lastname=? WHERE id_user=?";
+
+    public static final String SQL_GET_USER_FROM_TABLE = "SELECT * FROM users";
+
+    public static final String SQL_GET_USER_BY_LOGIN = "SELECT * FROM users WHERE login=(?)";
 
     private DBConstant() {
     }
