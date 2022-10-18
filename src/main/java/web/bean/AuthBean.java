2c0eedaa0fde881bc120c316de46d032803467b3
Vladyslav Batih <vladislavbatig@gmail.com>
2022-10-18 21:38:57 +0300

    Add bean classes

---
+++ src/main/java/web/bean/AuthBean.java
@@ -0,0 +1,34 @@
+package web.bean;
+
+import db.entity.User;
+
+public class AuthBean {
+
+    private String login;
+    private String password;
+    private User user;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/web/bean/RegistrationBean.java
@@ -0,0 +1,52 @@
+package web.bean;
+
+import db.entity.User;
+
+public class RegistrationBean {
+
+    private String login;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private User user;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
