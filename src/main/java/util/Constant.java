7cb9205c9b2ff36de262ad2e8f2f158afc2368d0
Vladyslav Batih <vladislavbatig@gmail.com>
2022-10-18 21:44:27 +0300

    Add util class

---
+++ src/main/java/util/Constant.java
@@ -0,0 +1,20 @@
+package util;
+
+public class Constant {
+
+    public static final String USER_SERVICE_MANAGER = "userManager";
+//    public static final String BILL_SERVICE_MANAGER = "billManager";
+//    public static final String CAR_SERVICE_MANAGER = "carManager";
+//    public static final String ORDER_SERVICE_MANAGER = "orderManager";
+
+    public static final String USER = "user";
+    public static final String UTIL = "util";
+
+    public static final String USER_ROLE = "userRole";
+
+    public static final String RESUBMITTING_PARAMETER = "check";
+    public static final String RESOURCE_BUNDLE = "resource";
+
+    private Constant() {
+    }
+}
